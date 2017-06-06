54aa07a4dc8c1ad1a7bef75f1d5815407f807bed
kamarulhilmi <kamarulhilmi@gmail.com>
2017-06-06 17:30:10 +0800

    Delete the selenium dependencies at the test cases.

--- SIPTests/TC02_LoginWithInvalidUsername.cs
+++ SIPTests/TC02_LoginWithInvalidUsername.cs
@@ -2,14 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Support;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium.Interactions;
 
 namespace SIPTests
 {
